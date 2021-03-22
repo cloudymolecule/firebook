d14b18746a9fcb5126a437c53dc5cda6eaf0b42b
Cloudymolecule <baires82@gmail.com>
2021-03-22 13:01:52 -0400

    Update Day class

---
+++ frontend/js/src/day.js
@@ -0,0 +1,11 @@
+class Day{
+    static all = []
+    
+    constructor({date, open_hour, close_hour}){
+        this.date = date
+        this.open_hour = open_hour
+        this.close_hour = close_hour
+
+        Day.all.push(this)
+    }
+}
