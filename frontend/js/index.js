32cede9378defe805b90d346c0752fb1b7defcbd
Cloudymolecule <baires82@gmail.com>
2021-03-22 12:14:59 -0400

    Add backend api

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,6 +1,6 @@
 
-let a = document.getElementById('create-edit-day')
-a.style.display = 'none'
+// let a = document.getElementById('create-edit-day')
+// a.style.display = 'none'
 
-let b = document.getElementById('book-appt')
-b.style.display = 'grid'
+// let b = document.getElementById('book-appt')
+// b.style.display = 'grid'
